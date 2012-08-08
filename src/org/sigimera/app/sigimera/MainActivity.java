5a4dc15eda1c85a3d8bc0562f1a92d876b54ddc0
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2012-08-08 17:13:49 +0200

    Show the last X crises in a list.

--- src/org/sigimera/app/sigimera/MainActivity.java
+++ src/org/sigimera/app/sigimera/MainActivity.java
@@ -1,5 +1,8 @@
 package org.sigimera.app.sigimera;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,32 +15,74 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
 
 	private SessionHandler session_handler;
+		
+	private OnItemClickListener listClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> list, View view, int position, long id) {
+			//TODO: show crises
+			System.out.println("CLICKED ON " + position);			
+		}
+	};	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);        
+        
     	this.session_handler = SessionHandler.getInstance(getSessionSettings());
 		try {
 			String auth_token = this.session_handler.getAuthenticationToken();
 			JSONArray crises = CrisesController.getInstance().getCrises(auth_token, 1);
-			for ( int count = 0; count < crises.length(); count++ ) {
-				try {
-					JSONObject crisis = (JSONObject) crises.get(count);
-					System.out.println("[" + count + "] " + crisis.getString("dc_title"));
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			
     		// 1. Get Crises List
     		// 1a. If crises list null set auth_token null and render R.layout.login
     		// 1b. If not null build the crises list in the activity_main
-    		setContentView(R.layout.activity_main);
+    		setContentView(R.layout.crises_list);
+    		
+    		ListView list = (ListView) findViewById(R.id.crises_list);
+    		list.setOnItemClickListener(this.listClickListener);
+    		
+    		ArrayList<HashMap<String, String>> buttonList = new ArrayList<HashMap<String, String>>();
+    		
+    		HashMap<String, String> map = new HashMap<String, String>();
+    		
+    		for ( int count = 0; count < crises.length(); count++ ) {
+				try {
+					JSONObject crisis = (JSONObject) crises.get(count);
+					
+					map = new HashMap<String, String>();		
+					map.put("top", crisis.getString("dc_title"));
+					
+					String crisis_type = crisis.getJSONArray("dc_subject").getString(0);
+					if ( crisis_type.contains("flood") )
+						map.put("icon", R.drawable.flood + "");
+					else if ( crisis_type.contains("earthquake") )
+						map.put("icon", R.drawable.earthquake + "");
+					else if ( crisis_type.contains("cyclone") )
+						map.put("icon", R.drawable.cyclone + "");
+					else if ( crisis_type.contains("volcano") )
+						map.put("icon", R.drawable.volcano + "");
+					
+					
+					buttonList.add(map);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+    		
+    		SimpleAdapter adapterMainList = new SimpleAdapter(this, buttonList, 
+    				R.layout.list_entry, new String[]{ "icon", "top" },
+    				new int[] { R.id.icon, R.id.topText });
+    		list.setAdapter(adapterMainList);
+    		
 		} catch (AuthenticationErrorException e) {
     		setContentView(R.layout.login);
 		}
