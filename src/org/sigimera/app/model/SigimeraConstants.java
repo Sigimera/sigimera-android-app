6d93c622f3c243bcfdcba1704fbb5afb7442770d
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2012-08-16 00:11:56 +0200

    Define a Constants class

---
+++ src/org/sigimera/app/model/SigimeraConstants.java
@@ -0,0 +1,19 @@
+package org.sigimera.app.model;
+
+/**
+ * Class which includes all the constants used in the Sigimera App.
+ * 
+ * @author Corneliu-Valentin Stanciu
+ *
+ */
+public abstract class SigimeraConstants {
+	
+	public static final String LOG_TAG_SIGIMERA_APP = "SigimeraApp";
+	public static final String LOG_TAG_LOCATION_CONTROLLER = "LocationController";
+	public static final String LOG_TAG_CONFIG = "Config";
+	
+	/**
+	 * Location radius in kilometres
+	 */
+	public static final String LOCATION_RADIUS = "200";
+}
