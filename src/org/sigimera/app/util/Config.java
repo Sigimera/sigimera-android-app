004fddc92998f1c47d05ab8c274665412eabf52d
Alex Oberhauser <alex.oberhauser@sigimera.org>
2012-08-15 14:52:45 +0200

    Improving the GCM implementation and sending now also the device name.

--- src/org/sigimera/app/util/Config.java
+++ src/org/sigimera/app/util/Config.java
@@ -3,49 +3,20 @@ package org.sigimera.app.util;
 /**
  * TODO: Out-source the configuration values to an external configuration file.
  */
-public class Config {			
+public abstract class Config {			
 	/**
 	 * <b>ATTENTION:</b> Change this only if you know what you are doing.
 	 */
 	public static final String LOG_TAG = "SigimeraAndroidApp";
 	
+	/**
+	 * The Sigimera Google Cloud Messaging project ID
+	 */
 	public static final String GCM_PROJECT_ID = null;
-			
-	private final String WWW_HOST = "http://172.16.1.28:3000/api/v1";
-	private final String API_HOST = "http://172.16.1.28:9292/v1";
 
 	/**
-	 * Singleton pattern 
+	 * The Sigimera Endpoints
 	 */
-	private static Config instance = null;
-	
-	public static Config getInstance() {
-		if ( instance == null ) instance = new Config();
-		return instance;
-	}
-
-	private Config() {
-//		try {
-//			settings = new Properties();
-//			FileInputStream fis = new FileInputStream("values/settings.xml");
-//			settings.loadFromXML(fis);
-//			
-//			WWW_HOST = settings.getProperty("www.host");
-//			API_HOST = settings.getProperty("api.host");
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	}
-
-	public String getAPIHost() {
-		return this.API_HOST;
-	}
-
-	public String getWWWHost() {
-		return this.WWW_HOST;
-	}
+	public final static String WWW_HOST = "http://172.16.1.28:3000/api/v1";
+	public final static String API_HOST = "http://172.16.1.28:9292/v1";
 }
