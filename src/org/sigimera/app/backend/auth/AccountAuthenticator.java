4cdffb344c2edc7e3f10799a38b9dd8e78e4c2c3
Alex Oberhauser <alex.oberhauser@sigimera.org>
2012-09-20 17:10:48 +0200

    Trying to open an activity if clicked on 'Add Account' under Android System Settings.

--- src/org/sigimera/app/backend/auth/AccountAuthenticator.java
+++ src/org/sigimera/app/backend/auth/AccountAuthenticator.java
@@ -6,6 +6,7 @@ import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -14,10 +15,11 @@ import android.os.Bundle;
  *
  */
 public class AccountAuthenticator extends AbstractAccountAuthenticator {
+	private final Context context;
 
-	public AccountAuthenticator(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
+	public AccountAuthenticator(Context _context) {
+		super(_context);
+		this.context = _context;
 	}
 
 	@Override
@@ -25,8 +27,15 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
 			String accountType, String authTokenType,
 			String[] requiredFeatures, Bundle options)
 			throws NetworkErrorException {
-		// TODO Auto-generated method stub
-		return null;
+        final Intent intent = new Intent(context, AuthenticatorActivity.class);
+        intent.putExtra(AuthenticatorActivity.PARAM_AUTHTOKEN_TYPE,
+            authTokenType);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE,
+            response);
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        return bundle;
+
 	}
 
 	@Override
