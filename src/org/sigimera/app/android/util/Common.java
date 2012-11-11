4bed1eef78165dcf48fe6ea110aa9ccadcabdc65
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2012-11-11 14:39:08 +0100

    Move the capitalize methond into Common class

--- src/org/sigimera/app/android/util/Common.java
+++ src/org/sigimera/app/android/util/Common.java
@@ -78,4 +78,9 @@ public class Common {
 		else 
 			return false;
 	}
+	
+	 public static String capitalize(String s) {
+	        if (s.length() == 0) return s;
+	        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	    }
 }
