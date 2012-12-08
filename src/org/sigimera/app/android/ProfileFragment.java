7a6ad944ebcbd19bfd02d1d4ea03398d861d6cbe
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2012-12-08 20:47:07 +0100

    Show the radius chooser seekbar, cache size and field for overwriting location

--- src/org/sigimera/app/android/ProfileFragment.java
+++ src/org/sigimera/app/android/ProfileFragment.java
@@ -10,6 +10,7 @@ import org.sigimera.app.android.controller.CrisesController;
 import org.sigimera.app.android.exception.AuthenticationErrorException;
 import org.sigimera.app.android.model.Constants;
 import org.sigimera.app.android.model.UsersStats;
+import org.sigimera.app.android.util.Common;
 import org.sigimera.app.android.util.MD5Util;
 
 import android.app.ProgressDialog;
@@ -23,7 +24,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
@@ -31,6 +37,10 @@ public class ProfileFragment extends Fragment {
 	private View view;
 	private Drawable drawable;
 	private String auth_token;
+	
+	private SeekBar nearCrisisRadius;
+	private TextView nearCrisisRadiusValue;
+	private TextView overwriteLocation;
 	
 	private UsersStats stats;
 	
@@ -52,7 +62,7 @@ public class ProfileFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.profile_fragment, container, false);		
+		view = inflater.inflate(R.layout.profile_fragment, container, false);
 
 		progessDialog = ProgressDialog.show(getActivity(),
 				"Preparing profile information!",
@@ -89,9 +99,21 @@ public class ProfileFragment extends Fragment {
 		return view;
 	}
 	
-	private void updateProfile() {		
+	private void updateProfile() {
+		long size = ApplicationController.getInstance().getCacheSize();
+		
+		StringBuffer content = new StringBuffer();
+		content.append("<p>");
+		content.append("<b>" + stats.getName() + "</b>");
+		content.append("<br/>");
+		content.append("<small><i>" + stats.getName() + "</i></small>");		
+		content.append("<br/>");
+		content.append("<br/>");
+		content.append("<small>" + "Used space: " + Common.transformTwoDecimalDoubleNumber(size / (1000.0 * 1000.0 * 1000.0)) + " Mb" + "</small>");
+		content.append("</p>");
+		
 		TextView name = (TextView) view.findViewById(R.id.name);
-		name.setText(Html.fromHtml("<p><b>" + stats.getName() + "</b><br/><small><i>" + stats.getUsername() +"</i></small></p>"));	
+		name.setText(Html.fromHtml(content.toString()));	
 		
 		TextView images = (TextView) view.findViewById(R.id.images);
 		images.setText(Html.fromHtml("<p><b>" + stats.getUploadedImages() + "</b><br/><small>Images</small></p>"));
@@ -108,6 +130,21 @@ public class ProfileFragment extends Fragment {
 		ImageView avatar = (ImageView) view.findViewById(R.id.avatar);
 		avatar.setImageDrawable(drawable);
 		
+		CheckBox enableNearCrises = (CheckBox) view.findViewById(R.id.enable_near_crises);
+		enableNearCrises.setOnCheckedChangeListener(checkedChangeListener);
+		
+		nearCrisisRadiusValue = (TextView) view.findViewById(R.id.near_crisis_radius_value);
+		nearCrisisRadiusValue.setText("Near crisis radius: " + Constants.LOCATION_RADIUS + " km");
+		
+		nearCrisisRadius = (SeekBar) view.findViewById(R.id.near_crisis_radius);
+		nearCrisisRadius.setOnSeekBarChangeListener(seekBarChangeListener);
+		nearCrisisRadius.setProgress(Constants.LOCATION_RADIUS);
+		
+		overwriteLocation = (TextView) view.findViewById(R.id.overwrite_location);
+		
+		// set the near crises view as "disabled"
+		disableNearCrisesView();
+		
 		progessDialog.dismiss();
 	}
 	
@@ -122,5 +159,41 @@ public class ProfileFragment extends Fragment {
 			}
 		}
 		return null;
+	}
+	
+	private OnCheckedChangeListener checkedChangeListener = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			if ( isChecked )
+				enableNearCrisesView();
+			else
+				disableNearCrisesView();
+		}
+	};
+	
+	private OnSeekBarChangeListener seekBarChangeListener = new OnSeekBarChangeListener() {
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser) {
+			nearCrisisRadiusValue.setText("Near crisis radius: " + progress + " km");
+		}
+		
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {}
+		
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+	};
+	
+	private void enableNearCrisesView() {
+		this.nearCrisisRadius.setEnabled(true);
+		this.nearCrisisRadiusValue.setEnabled(true);
+		this.overwriteLocation.setEnabled(true);
+	}
+	
+	private void disableNearCrisesView() {
+		this.nearCrisisRadius.setEnabled(false);
+		this.nearCrisisRadiusValue.setEnabled(false);
+		this.overwriteLocation.setEnabled(false);
 	}
 }
