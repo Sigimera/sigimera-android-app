6462913d1a5b464fdf43fa5ffd91671c0dc267a8
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2012-12-09 21:36:07 +0100

    Fix the saveing radius

--- src/org/sigimera/app/android/ProfileFragment.java
+++ src/org/sigimera/app/android/ProfileFragment.java
@@ -29,6 +29,7 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TabHost;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
@@ -37,15 +38,15 @@ public class ProfileFragment extends Fragment {
 	private View view;
 	private Drawable drawable;
 	private String auth_token;
-	
+
 	private CheckBox enableNearCrises;
 	private SeekBar nearCrisisRadius;
 	private TextView nearCrisisRadiusValue;
 	private TextView overwriteLocation;
-	
+
 	private UsersStats stats;
 	private int radius;
-	
+
 	private ProgressDialog progessDialog = null;
 
 	private final Handler guiHandler = new Handler();
@@ -72,22 +73,27 @@ public class ProfileFragment extends Fragment {
 		progessDialog.setCancelable(true);
 		Thread worker = new Thread() {
 			@Override
-			public void run() {				
+			public void run() {
 				try {
 					Looper.prepare();
-					auth_token = ApplicationController.getInstance().getSessionHandler().getAuthenticationToken();
-										
-					stats = CrisesController.getInstance().getUsersStats(auth_token);
-					radius = CrisesController.getInstance().getNearCrisesRadius();
-					
-					if (stats == null) Log.d("[PROFILE FRAGMENT]", "User stats are empty."); 
-					
-					if ( stats != null && stats.getUsername() != null) {
-						InputStream is = (InputStream) getAvatarURL(stats.getUsername()).getContent();
+					auth_token = ApplicationController.getInstance()
+							.getSessionHandler().getAuthenticationToken();
+
+					stats = CrisesController.getInstance().getUsersStats(
+							auth_token);
+					radius = CrisesController.getInstance()
+							.getNearCrisesRadius();
+
+					if (stats == null)
+						Log.d("[PROFILE FRAGMENT]", "User stats are empty.");
+
+					if (stats != null && stats.getUsername() != null) {
+						InputStream is = (InputStream) getAvatarURL(
+								stats.getUsername()).getContent();
 						drawable = Drawable.createFromStream(is, "src name");
 					}
-					
-					guiHandler.post(updateGUI);					
+
+					guiHandler.post(updateGUI);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -101,69 +107,85 @@ public class ProfileFragment extends Fragment {
 
 		return view;
 	}
-	
+
 	private void updateProfile() {
 		long size = ApplicationController.getInstance().getCacheSize();
-		
-		StringBuffer content = new StringBuffer();
-		content.append("<p>");
-		content.append("<b>" + stats.getName() + "</b>");
-		content.append("<br/>");
-		content.append("<small><i>" + stats.getName() + "</i></small>");		
-		content.append("<br/>");
-		content.append("<br/>");
-		content.append("<small>" + "Used space: " + Common.transformTwoDecimalDoubleNumber(size / (1000.0 * 1000.0 * 1000.0)) + " Mb" + "</small>");
-		content.append("</p>");
-		
-		TextView name = (TextView) view.findViewById(R.id.name);
-		name.setText(Html.fromHtml(content.toString()));	
-		
-		TextView images = (TextView) view.findViewById(R.id.images);
-		images.setText(Html.fromHtml("<p><b>" + stats.getUploadedImages() + "</b><br/><small>Images</small></p>"));
-		
-		TextView locations = (TextView) view.findViewById(R.id.location);
-		locations.setText(Html.fromHtml("<p><b>" + stats.getReportedLocations() + "</b><br/><small>Locations</small></p>"));
-		
-		TextView missingPeople = (TextView) view.findViewById(R.id.missing_people);
-		missingPeople.setText(Html.fromHtml("<p><b>" + stats.getReportedMissingPeople() + "</b><br/><small>Missing People</small></p>"));
-		
-		TextView comments = (TextView) view.findViewById(R.id.comments);
-		comments.setText(Html.fromHtml("<p><b>" + stats.getPostedComments() + "</b><br/><small>Comments</small></p>"));
-		
+
+		StringBuffer content;
+
+		if (stats != null) {
+			content = new StringBuffer();
+			content.append("<p>");
+			content.append("<b>" + stats.getName() + "</b>");
+			content.append("<br/>");
+			content.append("<small><i>" + stats.getName() + "</i></small>");
+			content.append("<br/>");
+			content.append("<br/>");
+			content.append("<small>"
+					+ "Used space: "
+					+ Common.transformTwoDecimalDoubleNumber(size
+							/ (1000.0 * 1000.0 * 1000.0)) + " Mb" + "</small>");
+			content.append("</p>");
+
+			TextView name = (TextView) view.findViewById(R.id.name);
+			name.setText(Html.fromHtml(content.toString()));
+
+			TextView images = (TextView) view.findViewById(R.id.images);
+			images.setText(Html.fromHtml("<p><b>" + stats.getUploadedImages()
+					+ "</b><br/><small>Images</small></p>"));
+
+			TextView locations = (TextView) view.findViewById(R.id.location);
+			locations.setText(Html.fromHtml("<p><b>"
+					+ stats.getReportedLocations()
+					+ "</b><br/><small>Locations</small></p>"));
+
+			TextView missingPeople = (TextView) view
+					.findViewById(R.id.missing_people);
+			missingPeople.setText(Html.fromHtml("<p><b>"
+					+ stats.getReportedMissingPeople()
+					+ "</b><br/><small>Missing People</small></p>"));
+
+			TextView comments = (TextView) view.findViewById(R.id.comments);
+			comments.setText(Html.fromHtml("<p><b>" + stats.getPostedComments()
+					+ "</b><br/><small>Comments</small></p>"));
+		}
+
 		ImageView avatar = (ImageView) view.findViewById(R.id.avatar);
 		avatar.setImageDrawable(drawable);
-		
+
 		content = new StringBuffer();
 		content.append("Enable crises near you");
 		content.append("<br />");
 		content.append("<small><small>CRISES window will list only crises in the selected radius</small></small>");
-		
-		enableNearCrises = (CheckBox) view.findViewById(R.id.enable_near_crises);
-		enableNearCrises.setOnCheckedChangeListener(checkedChangeListener);	
+
+		enableNearCrises = (CheckBox) view
+				.findViewById(R.id.enable_near_crises);
+		enableNearCrises.setOnCheckedChangeListener(checkedChangeListener);
 		enableNearCrises.setText(Html.fromHtml(content.toString()));
-		
-		nearCrisisRadiusValue = (TextView) view.findViewById(R.id.near_crisis_radius_value);		
-		
+
+		nearCrisisRadiusValue = (TextView) view
+				.findViewById(R.id.near_crisis_radius_value);
+
 		nearCrisisRadius = (SeekBar) view.findViewById(R.id.near_crisis_radius);
-		nearCrisisRadius.setOnSeekBarChangeListener(seekBarChangeListener);		
-		
-		overwriteLocation = (TextView) view.findViewById(R.id.overwrite_location);		
-		
-		if ( radius == 0 ){
+		nearCrisisRadius.setOnSeekBarChangeListener(seekBarChangeListener);
+
+		overwriteLocation = (TextView) view
+				.findViewById(R.id.overwrite_location);
+
+		if (radius == 0) {
 			disableNearCrisesView();
-			nearCrisisRadiusValue.setText("Near crisis radius: " + Constants.LOCATION_RADIUS + " km");
-			nearCrisisRadius.setProgress(Constants.LOCATION_RADIUS);
-		}else { 
+			radius = Constants.LOCATION_RADIUS;
+		} else
 			enableNearCrisesView();
-			nearCrisisRadiusValue.setText("Near crisis radius: " + radius + " km");
-			nearCrisisRadius.setProgress(radius);
-		}
+
+		nearCrisisRadiusValue.setText("Near crisis radius: " + radius + " km");
+		nearCrisisRadius.setProgress(radius);
 
 		progessDialog.dismiss();
 	}
-	
+
 	private URL getAvatarURL(String email) {
-		if ( email != null ) {
+		if (email != null) {
 			try {
 				String emailHash = MD5Util.md5Hex(email.toLowerCase().trim());
 				URL url = new URL("http://www.gravatar.com/avatar/" + emailHash);
@@ -174,42 +196,48 @@ public class ProfileFragment extends Fragment {
 		}
 		return null;
 	}
-	
+
 	private OnCheckedChangeListener checkedChangeListener = new OnCheckedChangeListener() {
 		@Override
-		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			if ( isChecked )
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+			if (isChecked)
 				enableNearCrisesView();
 			else
 				disableNearCrisesView();
 		}
 	};
-	
+
 	private OnSeekBarChangeListener seekBarChangeListener = new OnSeekBarChangeListener() {
 		int radiusProgress = 0;
+
 		@Override
 		public void onProgressChanged(SeekBar seekBar, int progress,
 				boolean fromUser) {
-			nearCrisisRadiusValue.setText("Near crisis radius: " + progress + " km");
+			nearCrisisRadiusValue.setText("Near crisis radius: " + progress
+					+ " km");
 			radiusProgress = progress;
 		}
-		
+
 		@Override
 		public void onStopTrackingTouch(SeekBar seekBar) {
 			CrisesController.getInstance().setNearCrisesRadius(radiusProgress);
 		}
-		
+
 		@Override
-		public void onStartTrackingTouch(SeekBar seekBar) {}
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
 	};
-	
+
 	private void enableNearCrisesView() {
 		this.nearCrisisRadius.setEnabled(true);
 		this.nearCrisisRadiusValue.setEnabled(true);
-		this.enableNearCrises.setChecked(true);	
+		this.enableNearCrises.setChecked(true);
 		this.overwriteLocation.setEnabled(true);
+		CrisesController.getInstance().setNearCrisesRadius(radius);
+		
 	}
-	
+
 	private void disableNearCrisesView() {
 		this.nearCrisisRadius.setEnabled(false);
 		this.nearCrisisRadiusValue.setEnabled(false);
