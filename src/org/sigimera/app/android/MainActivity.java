dbd4e609fbc9146ad7e36eb20a731408a563756f
Corneliu Stanciu <corneliu.stanciu@sigimera.org>
2013-01-18 22:39:13 +0100

    Remove the TabsAdapter inner class

--- src/org/sigimera/app/android/MainActivity.java
+++ src/org/sigimera/app/android/MainActivity.java
@@ -1,7 +1,5 @@
 package org.sigimera.app.android;
 
-import java.util.ArrayList;
-
 import org.sigimera.app.android.R;
 import org.sigimera.app.android.backend.network.LocationUpdaterHttpHelper;
 import org.sigimera.app.android.controller.ApplicationController;
@@ -16,7 +14,6 @@ import com.google.android.gcm.GCMRegistrar;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -27,23 +24,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.TabHost;
-import android.widget.TabWidget;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
@@ -131,7 +122,8 @@ public class MainActivity extends FragmentActivity {
 
 		try {
 			authToken = ApplicationController.getInstance().getSessionHandler()
-					.getAuthenticationToken();			
+					.getAuthenticationToken();
+			
 			setTabsAfterLogin();
 		} catch (AuthenticationErrorException e) {
 			setTabsBeforeLogin();
@@ -173,6 +165,7 @@ public class MainActivity extends FragmentActivity {
 						.getSessionHandler()
 						.login(emailView.getText().toString(),
 								passwordView.getText().toString())) {
+					
 					guiHandler.post(successfulLogin);
 				} else {
 					guiHandler.post(errorLogin);
@@ -301,6 +294,18 @@ public class MainActivity extends FragmentActivity {
 	}
 
 	private void setTabsAfterLogin() {
+		//Before setting the tabs get all information
+		try {
+			authToken = ApplicationController.getInstance().getSessionHandler().getAuthenticationToken();
+			PersistanceController.getInstance().updateEverything(authToken);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AuthenticationErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		this.mTabHost.clearAllTabs();
 		this.mTabsAdapter = new TabsAdapter(this, this.mTabHost,
 				this.mViewPager);
@@ -310,9 +315,6 @@ public class MainActivity extends FragmentActivity {
 				StatisticFragment.class, null);
 		this.mTabsAdapter.addTab(this.mTabHost.newTabSpec("Crises")
 				.setIndicator("Crises"), CrisesListFragment.class, null);
-//		if (CrisesController.getInstance().getNearCrisesRadius() != 0)
-//			this.mTabsAdapter.addTab(this.mTabHost.newTabSpec("Near you")
-//					.setIndicator("Near you"), CrisesListFragment.class, null);
 		this.mTabsAdapter.addTab(
 				mTabHost.newTabSpec("Profile").setIndicator("Profile"),
 				ProfileFragment.class, null);
@@ -329,123 +331,5 @@ public class MainActivity extends FragmentActivity {
 		mTabsAdapter.addTab(mTabHost.newTabSpec("Crises")
 				.setIndicator("Crises"), CrisesListFragment.class, null);
 		closeProgressDialog();
-	}
-
-	/**
-	 * This is a helper class that implements the management of tabs and all
-	 * details of connecting a ViewPager with associated TabHost. It relies on a
-	 * trick. Normally a tab host has a simple API for supplying a View or
-	 * Intent that each tab will show. This is not sufficient for switching
-	 * between pages. So instead we make the content part of the tab host 0dp
-	 * high (it is not shown) and the TabsAdapter supplies its own dummy view to
-	 * show as the tab content. It listens to changes in tabs, and takes care of
-	 * switch to the correct paged in the ViewPager whenever the selected tab
-	 * changes.
-	 */
-	public static class TabsAdapter extends FragmentPagerAdapter implements
-			TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
-		private final Context mContext;
-		private final TabHost mTabHost;
-		private final ViewPager mViewPager;
-		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-		static final class TabInfo {
-			// private final String tag;
-			private final Class<?> clss;
-			private final Bundle args;
-
-			TabInfo(String _tag, Class<?> _class, Bundle _args) {
-				// tag = _tag;
-				clss = _class;
-				args = _args;
-			}
-		}
-
-		static class DummyTabFactory implements TabHost.TabContentFactory {
-			private final Context mContext;
-
-			public DummyTabFactory(Context context) {
-				mContext = context;
-			}
-
-			@Override
-			public View createTabContent(String tag) {
-				View v = new View(mContext);
-				v.setMinimumWidth(0);
-				v.setMinimumHeight(0);
-				return v;
-			}
-		}
-
-		public TabsAdapter(FragmentActivity activity, TabHost tabHost,
-				ViewPager pager) {
-			super(activity.getSupportFragmentManager());
-			mContext = activity;
-			mTabHost = tabHost;
-			mViewPager = pager;
-			mTabHost.setOnTabChangedListener(this);
-			mViewPager.setAdapter(this);
-			mViewPager.setOnPageChangeListener(this);
-		}
-
-		public void addTab(TabHost.TabSpec tabSpec, Class<?> clss, Bundle args) {
-			tabSpec.setContent(new DummyTabFactory(mContext));
-			String tag = tabSpec.getTag();
-
-			TabInfo info = new TabInfo(tag, clss, args);
-			mTabs.add(info);
-			mTabHost.addTab(tabSpec);
-			notifyDataSetChanged();
-		}
-
-		@Override
-		public int getCount() {
-			return mTabs.size();
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			TabInfo info = mTabs.get(position);
-			return Fragment.instantiate(mContext, info.clss.getName(),
-					info.args);
-		}
-
-		@Override
-		public void onTabChanged(String tabId) {
-			int position = mTabHost.getCurrentTab();
-			mViewPager.setCurrentItem(position);
-		}
-
-		@Override
-		public void onPageScrolled(int position, float positionOffset,
-				int positionOffsetPixels) {
-		}
-
-		@Override
-		public void onPageSelected(int position) {
-			// Unfortunately when TabHost changes the current tab, it kindly
-			// also takes care of putting focus on it when not in touch mode.
-			// The jerk.
-			// This hack tries to prevent this from pulling focus out of our
-			// ViewPager.
-			TabWidget widget = mTabHost.getTabWidget();
-			int oldFocusability = widget.getDescendantFocusability();
-			widget.setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
-			mTabHost.setCurrentTab(position);
-			widget.setDescendantFocusability(oldFocusability);
-		}
-
-		@Override
-		public void onPageScrollStateChanged(int state) {
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			super.destroyItem(container, position, object);
-			FragmentManager manager = ((Fragment) object).getFragmentManager();
-			FragmentTransaction trans = manager.beginTransaction();
-			trans.remove((Fragment) object);
-			trans.commit();
-		}
 	}
 }
